9f35a9b9e02d5a38ff3b9983deae48e6d164f3b3
Uzair Nawaz <uzair.nawaz@utexas.edu>
2026-01-17 21:23:37 -0600

    add markdown render to nvim; kitty opens tmux by default

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -179,5 +179,16 @@ require("lazy").setup({
             vim.g.vimtex_view_general_viewer = 'zathura'
             vim.g.vimtex_view_general_options = '--synctex-forward @line:@col:@tex --fork @pdf'
         end
+    },
+
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
     }
+
 })
